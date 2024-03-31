fe37aaab0e2fde1673e529574e119973edcce7d4
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-03-31 16:20:26 +0900

    firstcommit:1.12.cpp

---
+++ apg4b/chapter01/1.12_EX12.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string S;
+	cin >> S;
+
+	int ans = 1;
+	for (int i = 1; i < S.size() - 1; i += 2)
+	{
+		if (S.(i) == '+') ans++;
+		else ans--;
+	}
+	cout << ans << endl;
+}
