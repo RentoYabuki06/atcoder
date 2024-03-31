f1bae64a22326fc6f55fbfa55334ffb6266063a3
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-03-31 13:35:37 +0900

    modified to modulo

--- ABC/abc347/c_idealHolidays.cpp
+++ ABC/abc347/c_idealHolidays.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int	main()
 {
-	int	N, A, B;
-	int D[200000];
+	int	N, A, B, max = 0, min = 2000000000, D;
 	cin >> N >> A >> B;
-	for (int i = 0; i < N; i++) cin >> D[i];
 
-	sort(D, D + N);
-	if ((D[N - 1] - D[0]) > A) cout << "Yes" << endl;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> D;
+		if (max < D % (A + B)) max = D % (A + B);
+		if (min > D % (A + B)) min = D % (A + B);
+	} 
+	if (max - min + 1 <= A) cout << "Yes" << endl;
 	else cout << "No" << endl;
+	return 0;
 }
