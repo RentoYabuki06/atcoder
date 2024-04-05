66e5ec54c22723d187ecd49aa15c34ffa5c48f85
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-05 10:47:24 +0900

    firstadd: 074b

---
+++ apg4b/chapter01/abc074b_CollectingBalls.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		int x;
+		cin >> x;
+		if (x < K - x)
+		{
+			sum += x * 2;
+		}
+		else{
+			sum += (K - x) * 2;
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
