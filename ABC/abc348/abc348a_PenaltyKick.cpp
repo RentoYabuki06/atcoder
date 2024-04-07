02aff12bdba29cbbb303f821562b59a61d410db8
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-07 10:37:57 +0900

    firstadd:348a

---
+++ ABC/abc348/abc348a_PenaltyKick.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int	main()
+{
+	int	N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		if ((i + 1) % 3 == 0)
+			cout << "x";
+		else
+			cout << "o";
+	}
+	cout <<endl;
+	return 0;
+}
