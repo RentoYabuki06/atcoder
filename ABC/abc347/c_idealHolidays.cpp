28627c53405eef8dd31af4a43eb1188de466541a
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-07 11:26:06 +0900

    onlysave

--- ABC/abc347/c_idealHolidays.cpp
+++ ABC/abc347/c_idealHolidays.cpp
@@ -3,16 +3,48 @@ using namespace std;
 
 int	main()
 {
-	int	N, A, B, max = 0, min = 2000000000, D;
+	int	N, A, B, D;
 	cin >> N >> A >> B;
-
+	int array[A + B] = {0};
 	for (int i = 0; i < N; i++)
 	{
 		cin >> D;
-		if (max < D % (A + B)) max = D % (A + B);
-		if (min > D % (A + B)) min = D % (A + B);
-	} 
-	if (max - min + 1 <= A) cout << "Yes" << endl;
+		array[D % (A + B)] = 1; 
+	}
+	int flag = 0;
+	int first = 0;
+	int first_flag = 0;
+	int count = 1;
+	int max = 0;
+	for (int i = 0; i < A + B; i++)
+	{
+		if (array[i] == 0 && flag == 1)
+			count++;
+		else if (array[i] == 0)
+		{
+			flag = 1;
+			count = 1;
+		}
+		else
+		{
+			if (first_flag == 0 && max > 0)
+			{
+				first_flag = 1;
+				first = max;
+			}
+			else flag = 0;
+		}
+		if (max < count) max = count; 
+	}
+	if (flag == 1) count += first;
+	int i = 0;
+	while (array[i] == 0)
+	{
+		count++;
+		i++;
+	}
+	if (max < count) max = count; 
+	if (max >= B) cout << "Yes" << endl;
 	else cout << "No" << endl;
 	return 0;
 }
