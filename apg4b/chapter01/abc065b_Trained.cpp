9c52b143939336e5e2edfe87cf9c7ff7d279a13b
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-07 11:40:05 +0900

    onlysave

--- apg4b/chapter01/abc065b_Trained.cpp
+++ apg4b/chapter01/abc065b_Trained.cpp
@@ -8,10 +8,10 @@ int main()
 	int N;
 	cin >> N;
 	int count = 1;
-	vector<int> button(N);
+	vector<int> button(N + 1);
 	for (int i = 1; i <= N; i++) cin >> button.at(i);
 	int next = 1;
-	for (int i = 0; i < 100100; i++)
+	for (int i = 0; i < 101010; i++)
 	{
 		next = button.at(next);
 		if (next == 2)
