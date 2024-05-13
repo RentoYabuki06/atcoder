91ab1556c7bcd84c6554bada3493f1a5e0c105b7
rento yabuki <rento.nky.1203@gmail.com>
2024-05-13 13:06:42 +0900

    firstadd

--- ABC/abc353/ABC353c_SigmaProblem.cpp
+++ ABC/abc353/ABC353c_SigmaProblem.cpp
@@ -12,11 +12,12 @@
 
 using namespace std;
 
-int N;
+long long N;
 long long A[300009];
+long long R[300009];
 vector<long long> Q;
 
-const long long INF = 100000000;
+const long long INF = 1e8;
 
 int main()
 {
@@ -26,20 +27,20 @@ int main()
 	{
 		cin >> A[i];
 		Q.push_back(A[i]);
-		total += A[i];
+		total += A[i] * (N - 1);
 	}
-	total *= (N - 1);
+	// 昇順に並べる
 	sort(Q.begin(), Q.end());
-	int count = 0;
+	// しゃくとり法
 	for (int i = 0; i < N - 1; i++)
 	{
-		for (int j = i + 1; j < N; j++)
-		{
-			auto tmp = lower_bound(Q.begin(), Q.end(), INF - Q[i]);
-			count += tmp - Q.begin();
-		}
+		if (i == 0) R[i] = 0;
+		else R[i] = R[i - 1];
+		// ギリギリまで増やす
+		while (R[i] < N - 1 - i && Q[R[i]] + Q[N - 1 - i] < INF) R[i]++;
+		total -= INF * ((N - 1 - i) - R[i]);
+		if (R[i] == N - 1 - i) break;
 	}
-	total -= count * INF;
 	cout << total << endl;
 	return 0;
 }
--- ABC/abc353/ABC353d_AnotherSigmaProblem.cpp
+++ ABC/abc353/ABC353d_AnotherSigmaProblem.cpp
@@ -12,24 +12,46 @@
 
 using namespace std;
 
-string S,T;	
+const long long INF = 998244353;
+
+long long N;
+long long A[200009];
+long long digits[200009];
+long long dig_mul[19];
 
 int main()
 {
-	cin >> S >> T;
-	for(int i = 0 ; i < S.size(); i++)
+	cin >> N;
+	long long total = 0;
+	for (int i = 0; i <= 10; i++) digits[i] = 0;
+	for (int i = 1; i <= N; i++)
 	{
-		for(int j = 0 ; j < T.size(); j++)
+		cin >> A[i];
+		digits[to_string(A[i]).size()]++; 
+		total += A[i] * (i - 1);
+		total %= INF;
+	}
+	// 10から10^9まで事前に計算しておく
+	dig_mul[0] = 1;
+	for (int i = 1; i <= 10; i++)
+	{
+		dig_mul[i] = 10 * dig_mul[i - 1];
+		dig_mul[i] %= INF;
+		digits[i] %= INF;
+	}
+	for (int i = 1; i <= N - 1; i++)
+	{
+		digits[to_string(A[i]).size()]--; 
+		A[i] %= INF;
+		for (int j = 1; j <= 10; j++)
 		{
-			if (S[i] == T[j])
-			{
-				cout << j+1;
-				i++;
-				cout << " ";
-			}
+			long long tmp = digits[j] * dig_mul[j];
+			tmp %= INF;
+			total += A[i] * tmp;
+			total %= INF;
 		}
 	}
-	cout << endl;
+	cout << total << endl;
 	return 0;
 }
 
