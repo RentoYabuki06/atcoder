44ed009bd9260bd5238d855a2af71d5825f2671c
rento yabuki <rento.nky.1203@gmail.com>
2024-06-09 19:37:05 +0900

    modified to AC

--- ABC/abc357/ABC357e_ReachabilityInFunctionalGraph.cpp
+++ ABC/abc357/ABC357e_ReachabilityInFunctionalGraph.cpp
@@ -1,88 +1,70 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <algorithm>
-#include <deque>
+#include <stack>
 
 using namespace std;
 
 long long N;
-long long A[200009];
-bool visited[200009];
-long long depth[200009];
-deque<long long> path_cycle;
+vector<long long> A;
+vector<long long> depth;
+vector<bool> visited, in_stack;
 
-bool ft_dfs(long long place) {
-    visited[place] = true;
-    path_cycle.push_back(place);
-    long long new_place = A[place];
-    if (new_place == place) {
-        depth[place] = 1;
-        path_cycle.pop_back();
-        return false; // 自己ループは無視
-    }
-    if (!visited[new_place])
-    {
-        if(ft_dfs(new_place)) return true;
-        else
-        {
-            path_cycle.clear();
-            return false;
+void dfs(long long v, stack<long long>& stk) {
+    visited[v] = true;
+    in_stack[v] = true;
+    stk.push(v);
+    
+    long long new_place = A[v];
+    if (!visited[new_place]) {
+        dfs(new_place, stk);
+    } else if (in_stack[new_place]) {
+        // 閉路検出
+        long long cycle_size = 0;
+        while (stk.top() != new_place) {
+            depth[stk.top()] = 0;
+            in_stack[stk.top()] = false;
+            stk.pop();
+            cycle_size++;
+        }
+        depth[new_place] = 0;
+        in_stack[new_place] = false;
+        stk.pop();
+        cycle_size++;
+        for (int i = 0; i < cycle_size; ++i) {
+            depth[new_place] = cycle_size;
+            new_place = A[new_place];
         }
     }
-    auto it = find(path_cycle.begin(), path_cycle.end(), new_place);
-    if (it != path_cycle.end()) {
-        while (new_place != path_cycle.front()) path_cycle.pop_front();
-        return true;
+    
+    if (!stk.empty() && depth[v] == -1) {
+        depth[v] = depth[A[v]] + 1;
+        in_stack[v] = false;
+        stk.pop();
     }
-    return false;
-}
-
-long long ft_depth(long long place) {
-    long long new_place = A[place];
-    if (new_place == place) return 1;
-    if (depth[new_place] == -1) {
-        depth[place] = ft_depth(new_place) + 1;
-    } else {
-        depth[place] = depth[new_place] + 1;
-    }
-    return depth[place];
 }
 
 int main() {
     // 入力
     cin >> N;
+    A.resize(N + 1);
+    depth.resize(N + 1, -1);
+    visited.resize(N + 1, false);
+    in_stack.resize(N + 1, false);
+    
     for (int i = 1; i <= N; i++) cin >> A[i];
-    // 初期化
+    
+    // DFSを用いて閉路を見つける
+    stack<long long> stk;
     for (int i = 1; i <= N; i++) {
-        visited[i] = false;
-        depth[i] = -1;
+        if (!visited[i]) {
+            dfs(i, stk);
+        }
     }
-    // 唯一存在する閉路を見つける
-    for (int i = 1; i <= N; i++) {
-        if (visited[i]) continue;
-        if(ft_dfs(i)) break;
-    }
-
-    // 閉路の中の頂点から到達可能な頂点数はcircle_sizeとなる
-    if (!path_cycle.empty())
-    {
-        long long circle_size = path_cycle.size();
-        for (int v : path_cycle) depth[v] = circle_size;
-    }
-    for (int i = 1; i <= N; i++) 
-    {
-        if (i == A[i]) depth[i] = 1;
-    }
-
-
+    
     // 解を求める
     long long ans = 0;
-    for (int i = 1; i <= N; i++) {
-        if (i == A[i]) ans++;
-        else if (depth[i] != -1) ans += depth[i];
-        else ans += ft_depth(i);
-    }
+    for (int i = 1; i <= N; i++) ans += depth[i];
     cout << ans << endl;
+    
     return 0;
 }
