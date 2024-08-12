0b8ea28cb72ab505b65a0530dafbc244dc8f172d
rento yabuki <rento.nky.1203@gmail.com>
2024-08-12 10:37:27 +0900

    modified: delete comment

--- ABC/abc361/ABC361c_MakeThemNarrow.cpp
+++ ABC/abc361/ABC361c_MakeThemNarrow.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <deque>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 typedef long long ll;
@@ -10,20 +11,21 @@ ll N, K;
 int main()
 {
     cin >> N >> K;
-    deque<ll> A(N);
+    vector<ll> A(N);
     for (int i = 0; i < N; i++) cin >> A[i];
     sort(A.begin(), A.end());
 
-    for (int i = 0; i < K; i++)
-    {
-        // AのサイズがN - iであることに注意して範囲を確認する
-        if (A[1] - A[0] > A[A.size() - 1] - A[A.size() - 2])
-            A.pop_front();
-        else
-            A.pop_back();
+    // K要素を削除した後のサイズ
+    ll remaining = N - K;
+
+    ll min_diff = A[remaining - 1] - A[0];
+    for (int i = 1; i + remaining - 1 < N; i++) {
+        ll diff = A[i + remaining - 1] - A[i];
+        if (diff < min_diff) {
+            min_diff = diff;
+        }
     }
 
-    // K個の要素を削除した後のサイズを考慮して、残りの要素を扱う
-    cout << A.back() - A.front() << endl;
+    cout << min_diff << endl;
     return 0;
 }
